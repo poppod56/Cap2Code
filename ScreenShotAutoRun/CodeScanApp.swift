2ccb10d6da3584411414f4fd0e0471334115ab83
poppod56 <pop.podmenyou@gmail.com>
2025-09-29 05:32:15 +0700

    [Change]: add fackbook sdk

--- ScreenShotAutoRun/CodeScanApp.swift
+++ ScreenShotAutoRun/CodeScanApp.swift
@@ -1,10 +1,29 @@
 import SwiftUI
+import FBSDKCoreKit
 
 @main
 struct CodeScanApp: App {
+    init() {
+        ApplicationDelegate.shared.application(
+            UIApplication.shared,
+            didFinishLaunchingWithOptions: nil
+        )
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL { url in
+                    ApplicationDelegate.shared.application(
+                        UIApplication.shared,
+                        open: url,
+                        sourceApplication: nil,
+                        annotation: [UIApplication.OpenURLOptionsKey.annotation]
+                    )
+                }
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                    AppEvents.shared.activateApp()
+                }
         }
     }
 }
